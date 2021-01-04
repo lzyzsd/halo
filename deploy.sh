13abf07fe31654ff21f93dbd83c6cb3425486ca6
lizhenyue3 <lizhenyue3@SHMAC-C02CX3XPZ.local>
2021-01-04 20:54:10 +0800

    scp 指定pub key文件

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ./gradlew clean bootjar
-scp ./build/libs/halo-1.2.0.jar root@122.51.146.42:/root/halo/
+scp -i ~/Downloads/brucetxkeychain ./build/libs/halo-1.2.0.jar root@122.51.146.42:/root/halo/
